6ffb83220b2dec11795588609bea556dcb3ce647
unknown <you@example.com>
2024-05-08 23:36:46 +0600

    iwefhweiralf

---
+++ main.js
@@ -0,0 +1,72 @@
+// let firstNama = "jihn";
+// let lastNama = "johon";
+// const person = {
+//   firstNama,
+//   lastNama,
+// };
+// console.log(person);
+// 222222
+// let n = prompt();
+// let obj = {};
+
+// for (let i = 1; i <= n; i++) {
+//   obj[i] = i * i;
+// }
+
+// console.log(obj);
+// 1111111
+// function displayObj(obj) {
+//   for (let key in obj) {
+//     console.log(`${key}: ${obj[key]}`);
+//   }
+// }
+// displayObj({
+//   firstName: "Diyorbek",
+//   lastName: "Bekmurodov",
+//   age: 20,
+//   languaues: ["java", "python", "c++"],
+//   friends: ["diyorbek", "Umarali", "Behruz", "Alpomish"],
+// });
+// 3- object
+// let n = prompt();
+// let obj = {};
+// for (let i = 1; i <= n; i++) {
+//   obj[i] = i * i;
+// }
+// console.log(obj);
+// 6-objecct
+// function defineObject(obj) {
+//     var keys = Object.keys(obj);
+
+//     for (var i = 0; i < keys.length; i++) {
+//         if (typeof obj[keys[i]] !== 'string') {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
+// var exampleObject = {
+//     key1: "value1",
+//     key2: "value2",
+//     key3: "value3"
+// };
+
+// console.log(defineObject(exampleObject)); /
+// 4 - objecttt
+// function createObjectFromArray(arr) {
+//     var obj = {};
+
+//     for (var i = 0; i < arr.length; i++) {
+//         var key = arr[i];
+//         var value = key.trim().length;
+
+//         obj[key] = value;
+//     }
+
+//     return obj;
+// }
+
+// var exampleArray = ["Sultonqul ", "Safarmurod","o'rol","Jahonger"];
+
+// console.log(createObjectFromArray(exampleArray));
